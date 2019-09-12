09a913fb2eefa93de5f75630e84b593f3c0c6b6f
Default name <2894519895@qq.com>
2019-09-12 21:37:04 +0800

    完成了产品展示页面
    可以点击产品发生跳转

--- products/models.py
+++ products/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
 
-	app_title = models.CharField(default='抖音：记录美好生活', max_length=50)
-	app_intro = models.TextField(default='这是APP介绍')
+	app_title = models.CharField(default='标题', max_length=50)
+	app_intro = models.TextField(default='介绍')
 	app_url = models.CharField(default='Http://', max_length=100)
 	app_icon = models.ImageField(default='default_icon.png', upload_to='images/')
 	app_image = models.ImageField(default='defult_image.png', upload_to='images/')
